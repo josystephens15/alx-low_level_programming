08b8fff877937aca37bb94501f40bc7eb1a9edc1
josystephens15 <josystephens15@gmail.com>
2022-09-11 11:06:18 -0700

    2nd If else

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
-nclude <stdio.h>
+#include <stdio.h>
+/**
+*main - main block
+*Return: 0
+*/
 
 int main(void)
 {
-		char ch;
+	char ch;
 
-			for (ch = 'a'; ch <= 'z'; ch++)
-					{
-								putchar(ch);
-									}
-				putchar('\n');
-					return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
