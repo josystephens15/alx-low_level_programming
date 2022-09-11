e5075c93b40e07c988e979f1fec7aea05afc43d3
josystephens15 <josystephens15@gmail.com>
2022-09-11 11:16:32 -0700

    3rd If else

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,17 @@
-nclude <stdio.h>
+#include <stdio.h>
+/**
+*main - main block
+*Return: 0
+*/
 
 int main(void)
 {
-		int ch;
+	int ch;
 
-			for (ch = 'a'; ch <= 'z'; ch++)
-						putchar(ch);
-				for (ch = 'A'; ch <= 'Z'; ch++)
-							putchar(ch);
-					putchar('\n');
-						return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
