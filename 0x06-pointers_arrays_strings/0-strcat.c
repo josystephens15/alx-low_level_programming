1c92477d9b35eff4979f4a2dd68b172c0a73e66a
josystephens15 <josystephens15@gmail.com>
2022-09-22 04:32:39 -0700

    0 Pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,17 +6,17 @@
  * @src: pointer to source input
  *
  * Return: pointer to resulting string @dest
- *
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
-	
+
 	c = 0;
 	/*find the size of dest array*/
 	while (dest[c])
 		c++;
+
 	/* iterate through each src array value without the null byte*/
 	for (c2 = 0; src[c2] ; c2++)
 /*append src[c2] to dest[c] while overwritting the null byte in dest*/
