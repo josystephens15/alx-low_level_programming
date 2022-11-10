be41e251395140519972e0bfa9d3e31aeda97bfb
josystephens15 <josystephens15@gmail.com>
2022-11-09 20:47:31 -0800

    Free malooc1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,32 +2,33 @@
 #include "main.h"
 
 /**
-* *_strdup - copies the string given as parameter
-* @str: string to duplicate
-*
-* Return: pointer to the copied string (Success), NULL (Error)
+ * *create_array - creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the array to create
+ * @c: char to initialize the array c
+ *
+ * Return: pointer to the array (Success), NULL (Error)
 */
-char *_strdup(char *str)
+char *create_array(unsigned int size, char c)
 {
-	char *dup;
-	unsigned int i, len;
+	char *p;
+	unsigned int i = 0;
 
-	i = 0;
-	len = 0;
-
-	if (str == NULL)
+	if (size == 0)
 		return (NULL);
 
-	while (str[len])
-		len++;
+	p = (char *) malloc(sizeof(char) * size);
 
-	dup = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+		return (0);
 
-	if (dup == NULL)
-		return (NULL);
-
-	while ((dup[i] = str[i]) != '\0')
+	while (i < size)
+	{
+		*(p + i) = c;
 		i++;
+	}
 
-	return (dup);
+	*(p + i) = '\0';
+
+	return (p);
 }
